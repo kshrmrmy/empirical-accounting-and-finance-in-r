912a30415503a7c970d07488207066a5f69812c4
muramiya <muramiya@econ.osaka-u.ac.jp>
2022-07-14 14:16:15 +0900

    Update ch04_24.R

--- ch04/ch04_24.R
+++ ch04/ch04_24.R
@@ -5,4 +5,5 @@ financial_data %>% summarize(mean_sales = mean(sales)) # summarize(financial_dat
 # A tibble: 1 x 1
 ## mean_sales
 ## <dbl>
-##   1    1520.647
+##   1    1520.647 （本文165頁では左記のように記していますが，正しくは以下の通りです）
+##   1    166027.
