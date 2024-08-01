870abde1e69f3a9f0e0731e99c96f73d3530deb3
Akitada Kasahara and Katsuhiko Muramiya <107167516+kshrmrmy@users.noreply.github.com>
2024-08-01 15:20:07 +0900

    Update ch03_58.R

--- ch03/ch03_58.R
+++ ch03/ch03_58.R
@@ -6,6 +6,6 @@ ggplot(data = figure_data) +
   labs(x = "Initial Cost", y = "IRR", title = "Initial Cost and IRR") +
   theme_classic() +
   annotate(geom = "text", x = 99, y = 0.05, label = "Initial Cost 100") + # 位置を指定して文字列を追加
-  annotate(geom = "segment", x = 100, xend = 100, y = 0.04, yend = 0.01, color = "black", size = 0.5, arrow = arrow(length = unit(0.3, "cm"))) # 始点や終点などを指定して矢印を追加
+  annotate(geom = "segment", x = 100, xend = 100, y = 0.04, yend = 0.01, color = "black", linewidth = 0.5, arrow = arrow(length = unit(0.3, "cm"))) # 始点や終点などを指定して矢印を追加
 
 ggsave("IRR.pdf", width = 20, height = 10, units = "cm") # 完成した図をPDF型式で保存
